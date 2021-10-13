d7ff888c0bf56b581a9991545d2aeef5c4384ab1
dudcha <odudchenko@icloud.com>
2021-10-13 02:14:18 -0500

    Add -q option for no output editing

--- edit/edit-mnd-according-to-new-cprops.sh
+++ edit/edit-mnd-according-to-new-cprops.sh
@@ -2,6 +2,21 @@
 # Wrapper around gnu parallel for editing the mnd in case all of apply-edits is not needed. Not employed at the moment.
 # Written by: OD
 
+USAGE=""
+
+quiet=false
+
+while getopts "q" opt; do
+case $opt in
+	q) quiet=true
+	;;
+	*) echo "$USAGE" >&2
+		exit 1
+	;;
+esac
+done
+shift $(( OPTIND-1 ))
+
 ##	GNU Parallel Dependency
 parallel="false"
 if hash parallel 2>/dev/null; then
@@ -18,4 +33,6 @@ mnd=$2
 pipeline=`cd "$( dirname $0)" && cd .. && pwd`
 edit_mnd_script=${pipeline}/edit/edit-mnd-according-to-new-cprops.awk
 
-[ $parallel == "true" ] && parallel -a ${mnd} --pipepart --will-cite --jobs 80% --block 1G "awk -f ${edit_mnd_script} ${new_cprops} - " || awk -f ${edit_mnd_script} ${new_cprops} ${mnd}
+[ "$quiet" == "true" ] && opt="-v label1=:::fragment_ -v label2=:::debris" || opt=""
+
+[ $parallel == "true" ] && parallel -a ${mnd} --pipepart --will-cite --jobs 80% --block 1G "awk $opt -f ${edit_mnd_script} ${new_cprops} - " || awk $opt -f ${edit_mnd_script} ${new_cprops} ${mnd}
